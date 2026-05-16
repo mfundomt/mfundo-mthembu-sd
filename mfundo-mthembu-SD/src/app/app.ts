3c61fb7b0421032afbf73ca646188c581cdd3d8b
Mfundomt <Mfundo.Mthembu@idmgroup.co.za>
2026-05-16 11:05:35 +0200

    add Angular Material theming and update styles; modify app structure for command components

--- mfundo-mthembu-SD/src/app/app.ts
+++ mfundo-mthembu-SD/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { Terminal } from './components/terminal/terminal/terminal';
+import { CommandTerminal } from './components/command-terminal/command-terminal';
 
 @Component({
   selector: 'app-root',
-  imports: [Terminal, RouterOutlet, CommonModule, RouterModule],
+  imports: [CommandTerminal, RouterOutlet, CommonModule, RouterModule],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
